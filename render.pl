0a0770ff7c4d9f18b0790dfec27bdf659e922d20
Gyusun Yeom <omniavinco@gmail.com>
2016-11-03 03:22:41 +0900

    Initial Commit

---
+++ render.pl
@@ -0,0 +1,3 @@
+﻿# render resume
+`latexml resume/$_.tex --destination=resume.xml && latexmlpost --dest=out/resume/resume-$_.html resume.xml --css=resume.css` for ("en", "kr", "jp");
+`rm resume.xml`;

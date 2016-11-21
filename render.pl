198022f38335f9236f3431d48ae9d1c113f88c80
Gyusun Yeom <omniavinco@gmail.com>
2016-11-21 18:55:10 +0900

    fix encoding issue

--- render.pl
+++ render.pl
@@ -1,4 +1,16 @@
-﻿# render resume
-`latexml resume/$_.tex --destination=resume.xml && latexmlpost --dest=out/resume/resume-$_.html resume.xml --css=resume.css` for ("en", "kr", "jp");
-`rm resume.xml`;
+# render resume
+sub latexml {
+  $in=shift;
+  $out=shift;
+  $css=shift;
+  if ($css) {
+    $css = "--css=$css";
+  }
+  print "run latexml for $in";
+  `latexml --inputencoding=utf-8 $in --destination=tmp.xml && latexmlpost --dest=$out tmp.xml $css`;
+}
+
+latexml("resume/$_.tex", "out/resume/resume-$_.html", "resume.css") for ("en", "kr", "jp");
+`rm tmp.xml`;
+print "copy resume.css";
 `cp resume/resume.css out/resume/resume.css`;

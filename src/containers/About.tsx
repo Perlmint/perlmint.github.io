0f5d02d6275fe3fe5dc1caf8326a33d90e9866f0
Gyusun Yeom <omniavinco@gmail.com>
2019-02-27 00:02:48 +0900

    New about layout

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -45,58 +45,64 @@ class AboutPage extends React.Component<IAboutProps, IAboutState> {
                     <title>About</title>
                 </Head>
                 <Grid container spacing={16}>
-                    <Grid item xs={12} md={6}>
-                        <Paper>
-                            <Typography
-                                className={classes.secionHeading}
-                                component='div'
-                                variant='headline'
-                            >
-                                Summary
-                            </Typography>
-                            <List component='nav'>
-                                {summary.map(([key, value], idx) => <ListItem key={idx}>
-                                    <ListItemText
-                                        primary={capitalize(key)}
-                                        secondary={<span>{(typeof value === 'string') ?
-                                            value : reduce<string, React.ReactNode[]>(
-                                                value,
-                                                (ret, item, iidx) => (ret.push(
-                                                    <Chip component='span' key={iidx} label={item} />,
-                                                    ' ',
-                                                ), ret),
-                                                []
-                                            )}
-                                        </span>} />
-                                </ListItem>)}
-                            </List>
-                        </Paper>
+                    <Grid item xs={false} md={1} lg={3}></Grid>
+                    <Grid item xs={12} md={10} lg={6} >
+                        <Grid container spacing={16}>
+                            <Grid item xs={12}>
+                                <Paper>
+                                    <Typography
+                                        className={classes.secionHeading}
+                                        component='div'
+                                        variant='headline'
+                                    >
+                                        Summary
+                                    </Typography>
+                                    <List component='nav'>
+                                        {summary.map(([key, value], idx) => <ListItem key={idx}>
+                                            <ListItemText
+                                                primary={capitalize(key)}
+                                                secondary={<span>{(typeof value === 'string') ?
+                                                    value : reduce<string, React.ReactNode[]>(
+                                                        value,
+                                                        (ret, item, iidx) => (ret.push(
+                                                            <Chip component='span' key={iidx} label={item} />,
+                                                            ' ',
+                                                        ), ret),
+                                                        []
+                                                    )}
+                                                </span>} />
+                                        </ListItem>)}
+                                    </List>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Paper>
+                                    <Typography
+                                        className={classes.secionHeading}
+                                        component='div'
+                                        variant='headline'
+                                        gutterBottom
+                                    >
+                                        Career
+                                    </Typography>
+                                    {careers.map((career, idx) => <CareerItem career={career} key={idx} />)}
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Paper>
+                                    <Typography
+                                        className={classes.secionHeading}
+                                        component='div'
+                                        variant='headline'
+                                    >
+                                        Links
+                                    </Typography>
+                                    <Links links={links} />
+                                </Paper>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Paper>
-                            <Typography
-                                className={classes.secionHeading}
-                                component='div'
-                                variant='headline'
-                                gutterBottom
-                            >
-                                Career
-                            </Typography>
-                            {careers.map((career, idx) => <CareerItem career={career} key={idx} />)}
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <Paper>
-                            <Typography
-                                className={classes.secionHeading}
-                                component='div'
-                                variant='headline'
-                            >
-                                Links
-                            </Typography>
-                            <Links links={links} />
-                        </Paper>
-                    </Grid>
+                    <Grid item xs={false} md={1} lg={3}></Grid>
                 </Grid>
             </div>}
         />;
